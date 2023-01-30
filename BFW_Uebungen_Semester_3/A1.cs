511e936099529cd2c7587456e890b6a99d55d606
gabr1eL44 <gabrielinc@gmx.de>
2023-01-30 17:52:56 +0100

    Initial commit

---
+++ BFW_Uebungen_Semester_3/A1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BFW_Uebungen_Semester_3
+{
+	public class A1
+	{
+		public static void Start()
+		{
+			Console.WriteLine("Willkommen im 3. Semester!");
+		}
+	}
+}
+
---
+++ BFW_Uebungen_Semester_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BFW_Uebungen_Semester_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start Current Task
+            A1.Start();
+
+            // Wait for KeyPress
+            Console.WriteLine("\nDrücken Sie eine beliebige Taste, um dieses Fenster zu schließen.");
+            ConsoleKeyInfo taste = Console.ReadKey();
+            if (taste.Key == ConsoleKey.Enter)
+                return;
+        }
+    }
+}
